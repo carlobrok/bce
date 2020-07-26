8034b550fb16c80323244fcd14144df409b6e518
carlobrok <carlo.brokering@outlook.de>
2020-07-26 23:41:32 +0200

    build folder, ignore build executable

---
+++ pi_build/objects.mk
@@ -0,0 +1,3 @@
+USER_OBJS :=
+
+LIBS := -lpthread -lboost_system -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -li2c
---
+++ pi_build/pi_src/subdir.mk
@@ -0,0 +1,28 @@
+# Add inputs and outputs from these tool invocations to the build variables
+CPP_SRCS += \
+../pi_src/ebc_lane_detection.cpp \
+../pi_src/CameraCapture.cpp \
+../pi_src/VideoServer.cpp \
+../pi_src/devices.cpp \
+
+
+OBJS += \
+./pi_src/ebc_lane_detection.o \
+./pi_src/CameraCapture.o \
+./pi_src/VideoServer.o \
+./pi_src/devices.o \
+
+CPP_DEPS += \
+./pi_src/ebc_lane_detection.d \
+./pi_src/CameraCapture.d \
+./pi_src/VideoServer.d \
+./pi_src/devices.d \
+
+
+# Each subdirectory must supply rules for building sources it contributes
+pi_src/%.o: ../pi_src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++17 -O3 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
