3fe908ad0110a61e2450e956fbfa54cd74881441
carlobrok <carlo.brokering@outlook.de>
2020-09-07 17:20:19 +0200

    add config

---
+++ pi_src/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Debug defines
+
+#define VISUAL_DEBUG		// Wenn definiert, werden Debuglinien und Konturen in img_rgb gezeichnet
+#define LOG_FILE			// Wenn definiert, versucht das Programm alle Sensordaten in eine, Outputdatei zu loggen, wenn der Name übergeben wurde
+
+
+// Allgemeine defines
+
+#define IMG_HEIGHT 480
+#define IMG_WIDTH 640
+
+
+
+
+#endif
