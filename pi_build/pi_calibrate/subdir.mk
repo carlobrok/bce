65c6bc4c4f8a0e0a0c9a06a484447a2d216aa4b7
carlobrok <carlo.brokering@outlook.de>
2020-09-28 22:24:34 +0200

    corrected dependencies

--- pi_build/pi_calibrate/subdir.mk
+++ pi_build/pi_calibrate/subdir.mk
@@ -1,14 +1,14 @@
 # Add inputs and outputs from these tool invocations to the build variables
 CPP_SRCS_EBC_CAL := \
-../pi_calibrate/ebc_lane_detection.cpp \
+../pi_calibrate/ebc_calibrate.cpp \
 ../pi_calibrate/VideoClient.cpp \
 
 OBJS_EBC_CAL := \
-./pi_calibrate/ebc_lane_detection.o \
+./pi_calibrate/ebc_calibrate.o \
 ./pi_calibrate/VideoClient.o \
 
 CPP_DEPS_EBC_CAL := \
-./pi_calibrate/ebc_lane_detection.d \
+./pi_calibrate/ebc_calibrate.d \
 ./pi_calibrate/VideoClient.d \
 
 # Each subdirectory must supply rules for building sources it contributes
