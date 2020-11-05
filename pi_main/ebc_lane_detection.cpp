30dd5d42405ed27753a5ab92f2fa2837bb3de96c
carlobrok <carlo.brokering@outlook.de>
2020-11-05 11:49:00 +0100

    set min max values

--- pi_main/ebc_lane_detection.cpp
+++ pi_main/ebc_lane_detection.cpp
@@ -65,7 +65,7 @@ int main() {
 
 
     // color filtering
-    color_filtering(warped, binary_line, cv::Scalar(), cv::Scalar());
+    color_filtering(warped, binary_line, cv::Scalar(121,130,108), cv::Scalar(126, 205, 190));
     srv::imshow("binary_line", binary_line);
     auto tcolor = std::chrono::system_clock::now();
     std::cout << "color filtering: " << std::chrono::duration_cast<std::chrono::milliseconds>(tcolor - tpersp_warp).count() << "ms" << std::endl;
