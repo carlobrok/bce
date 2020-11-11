c88fea2e53c67e791b1f7143c2ee1a4b8de0b9ba
bjoernell <bjoern.ellwert@gmail.com>
2020-11-11 22:12:07 +0100

    USSen Class added

--- arduino_us/arduino_us.ino
+++ arduino_us/arduino_us.ino
@@ -1,26 +1,31 @@
-enum pins_left {
-  L_ECHO,
-  L_TRIG,
-  L_OUT
-};
+#include "UsSen.h"
+#include "BuzzSpeaker.h"
 
-enum pins_left_mid {
-  LM_ECHO,
-  LM_TRIG,
-  LM_OUT
-};
+#define reUnTrig 13
+#define reUnEch 12
 
-enum pins_right_mid {
-  RM_ECHO,
-  RM_TRIG,
-  RM_OUT
-};
+#define liUnTrig 11
+#define liUnEch 10
 
-enum pins_right {
-  R_ECHO,
-  R_TRIG,
-  R_OUT
-};
+#define reObTrig 9
+#define reObEch 8
+
+#define liObTrig 6
+#define liObEch 7
+
+#define speakerPin 5
+
+String reUnName = "ReUn";
+String liUnName = "LiUn";
+String reObName = "ReOb";
+String liObName = "LiOb";
+
+UltrasonicSensor UsLiOb(liObTrig, liObEch, liObName);
+UltrasonicSensor UsReOb(reObTrig, reObEch, reObName);
+UltrasonicSensor UsLiUn(liUnTrig, liUnEch, liUnName);
+UltrasonicSensor UsReUn(reUnTrig, reUnEch, reUnName);
+
+BuzzSpeaker MainSpeaker(speakerPin);
 
 float dist_l, dist_lm, dist_rm, dist_r;
 String str_out = "";
@@ -28,55 +33,35 @@ String str_out = "";
 const int buzzer_pin = 5;
 unsigned long last_buzzer = 0;
 
-float get_distance(int trigger_pin, int echo_pin, int max_cm = 100);
+void usSenCheck(double distance)
+{
+  if (distance != 0)
+  {
+    Serial.println(distance);
+  }
+  if (distance < 100 && distance > 0)
+  {
+    MainSpeaker.playSound(2000, distance, 1);
+    Serial.println(distance);
+  }
+  if (distance == 0)
+  {
+    Serial.println("timeout");
+  }
+}
 
 void setup() {
-  pinMode(L_TRIG, OUTPUT);
-  pinMode(LM_TRIG, OUTPUT);
-  pinMode(RM_TRIG, OUTPUT);
-  pinMode(R_TRIG, OUTPUT);
-
-  pinMode(L_OUT, OUTPUT);
-  pinMode(LM_OUT, OUTPUT);
-  pinMode(RM_OUT, OUTPUT);
-  pinMode(R_OUT, OUTPUT);
-
   Serial.begin(115200);
 }
 
 void loop() {
 // read distances of ultrasonic sensors
-  dist_l = get_distance(L_TRIG, L_ECHO);
-  dist_lm = get_distance(LM_TRIG, LM_ECHO);
-  dist_rm = get_distance(RM_TRIG, RM_ECHO);
-  dist_r = get_distance(R_TRIG, R_ECHO);
-
+  usSenCheck(UsReOb.distance()); 
+  usSenCheck(UsLiOb.distance());
+  usSenCheck(UsReUn.distance());
+  usSenCheck(UsLiUn.distance());
 // write distances to serial port
-  str_out = String(dist_l,1) + ";" + String(dist_lm,1) + ";" + String(dist_rm,1) + ";" + String(dist_r,1);
-  Serial.println(str_out);
+  /*str_out = String(dist_l,1) + ";" + String(dist_lm,1) + ";" + String(dist_rm,1) + ";" + String(dist_r,1);
+  Serial.println(str_out);*/
 
-// beep when low distance
-  if((dist_l < 50 && dist_l != 0) || (dist_lm < 50 && dist_lm != 0) || (dist_rm < 50 && dist_rm != 0) || (dist_r < 50 && dist_r != 0)) {
-    if(millis() - last_buzzer > 200) {
-      last_buzzer = millis();
-    }
-    if(millis() - last_buzzer < 100) {
-      tone(buzzer_pin, 590);
-    } else {
-      noTone(buzzer_pin);
-    }
-  } else {
-    noTone(buzzer_pin);
-  }
-}
-
-float get_distance(int trigger_pin, int echo_pin, int max_cm) {
-  digitalWrite(trigger_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger_pin, LOW);
-  unsigned long duration = pulseIn(echo_pin, HIGH, (max_cm*2/0.034));
-
-  return duration*0.034/2;
 }
