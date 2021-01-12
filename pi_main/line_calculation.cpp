70146f484ef6955fc5e5265c307c40ef408ded27
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 00:02:22 +0100

    flush ostream to debug

--- pi_main/line_calculation.cpp
+++ pi_main/line_calculation.cpp
@@ -93,7 +93,7 @@ void roi_search(cv::Mat & binary_line, cv::Mat & vis_draw, lane_data & lane_mid,
 		cv::Rect rect_roi_left(0, binary_line.rows - (i_roi + 1) * roi_height, p_mid.x, roi_height);
 		cv::Rect rect_roi_right(p_mid.x, binary_line.rows - (i_roi + 1) * roi_height, binary_line.cols - p_mid.x, roi_height);
 
-		std::cout << "i=" << i_roi << " / left: " << rect_roi_left << " / right: " << rect_roi_right << " // area; point: ";
+		std::cout << "i=" << i_roi << " / left: " << rect_roi_left << " / right: " << rect_roi_right << " // area; point: " << std::flush;
 
 		// Ablauf:
 			// mat vom roi bereich erstellen
